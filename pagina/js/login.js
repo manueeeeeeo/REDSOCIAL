033fc28c48dd415bf80e7722240ee633d69190d8
manueeeeeeo <146026153+manueeeeeeo@users.noreply.github.com>
2024-05-26 12:46:02 +0200

    Update login.js

--- pagina/js/login.js
+++ pagina/js/login.js
@@ -1,1 +1,13 @@
+import { ManageAccount } from './firebaseconect.js';
 
+document.getElementById("sign").addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    const email = document.getElementById("correo1").value;
+    const password = document.getElementById("login_password").value;
+
+    const account = new ManageAccount();
+    account.authenticate(email, password);
+});
+
+console.log('Formulario de Inicio de Sesión');
