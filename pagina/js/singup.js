27a1b09639f8e4ae75c2e26ed99d30533a4cc941
manueeeeeeo <146026153+manueeeeeeo@users.noreply.github.com>
2024-05-29 18:05:43 +0200

    Update singup.js

--- pagina/js/singup.js
+++ pagina/js/singup.js
@@ -1,1 +1,13 @@
+import { ManageAccount } from './firebaseconect.js';
 
+document.getElementById("create").addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+
+    const account = new ManageAccount();
+    account.register(email, password);
+});
+
+console.log('Formulario de Registro');
